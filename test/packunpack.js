f0f5f8fe84850cbd85ccf44cb05d7aa1942c8546
Kaspars Foigts <laacz@laacz.lv>
2011-05-05 23:45:04 +0300

    PDU encoding from struct to Buffer.

--- test/packunpack.js
+++ test/packunpack.js
@@ -15,7 +15,9 @@ var unpack = packunpack.unpack;
 assert.strictEqual(pack('nc*', 0x1234, 65, 66).toString('binary'), "\x12\x34\x41\x42");
 assert.strictEqual(pack('N', 167).toString('binary'), "\x00\x00\x00\xa7");
 assert.strictEqual(pack('N', 1677866).toString('binary'), "\x00\x19\x9a\x2a");
+assert.strictEqual(pack('N', 1678191).toString('binary'), "\x00\x19\x9b\x6f");
 
 assert.deepEqual(unpack('nc*', "\x12\x34\x41\x42"), [0x1234, 65, 66]);
-assert.deepEqual(unpack('N', "\x00\x00\x00\xa7"), [167]);
-assert.deepEqual(unpack('N', "\x00\x19\x9a\x2a"), [1677866]);
+assert.deepEqual(unpack('N', "\x00\x00\x00\xa7"), 167);
+assert.deepEqual(unpack('N', "\x00\x19\x9a\x2a"), 1677866);
+assert.deepEqual(unpack('N', "\x00\x19\x9b\x6f"), 1678191);
